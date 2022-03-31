18f5aa9524306b4529934989172dadeacd4a5704
Raymond Staniek <rstani20@student.aau.dk>
2022-03-31 17:41:33 +0200

    android studio app setup

---
+++ android/app/src/main/java/dk/mths/jomo/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package dk.mths.jomo.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
