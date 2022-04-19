5200fca1aa783e24d3718deb59ec0edff71dec88
Raymond Staniek <rstani20@student.aau.dk>
2022-04-19 22:32:41 +0200

    added uSeR tRaCkInG~~ 👻💀

--- android/app/src/main/java/dk/mths/jomo/MainActivity.kt
+++ android/app/src/main/java/dk/mths/jomo/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.firebase.ktx.Firebase
 import dk.mths.jomo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
--- android/app/src/main/java/dk/mths/jomo/ui/dashboard/DashboardFragment.kt
+++ android/app/src/main/java/dk/mths/jomo/ui/dashboard/DashboardFragment.kt
@@ -1,16 +1,19 @@
 package dk.mths.jomo.ui.dashboard
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Switch
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.switchmaterial.SwitchMaterial
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import dk.mths.jomo.databinding.FragmentDashboardBinding
+import java.time.Instant
+import java.time.format.DateTimeFormatter
 
 class DashboardFragment : Fragment() {
 
@@ -19,6 +22,8 @@ class DashboardFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private val db = Firebase.firestore
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -34,14 +39,34 @@ class DashboardFragment : Fragment() {
         val switchControl: SwitchMaterial = binding.switch1
         switchControl.setOnClickListener {
             if (switchControl.isChecked){
-                writeSetting("accessibility_display_daltonizer_enabled", "1")
-                writeSetting("accessibility_display_daltonizer", "0")
+                handleOnGrayscaleActivated()
             } else {
-                writeSetting("accessibility_display_daltonizer_enabled", "0")
-                writeSetting("accessibility_display_daltonizer", "-1")
+                handleOnGrayscaleDeactivated()
             }
+
+            val logMessage = composeGrayscaleOpLogMsg(switchControl.isChecked)
+            db.collection("test").add(logMessage)
         }
         return root
+    }
+
+    private fun handleOnGrayscaleActivated() {
+        writeSetting("accessibility_display_daltonizer_enabled", "1")
+        writeSetting("accessibility_display_daltonizer", "0")
+    }
+
+    private fun handleOnGrayscaleDeactivated() {
+        writeSetting("accessibility_display_daltonizer_enabled", "0")
+        writeSetting("accessibility_display_daltonizer", "-1")
+    }
+
+    @SuppressLint("HardwareIds")
+    private fun composeGrayscaleOpLogMsg(enabled: Boolean): HashMap<String, String> {
+        return hashMapOf(
+            "daltonizer" to enabled.toString(),
+            "timestamp" to DateTimeFormatter.ISO_INSTANT.format(Instant.now()),
+            "uid" to Settings.Secure.getString(requireActivity().contentResolver, Settings.Secure.ANDROID_ID)
+        )
     }
 
     override fun onDestroyView() {
