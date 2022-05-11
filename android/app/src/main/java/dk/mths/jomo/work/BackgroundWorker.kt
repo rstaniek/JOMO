0cea284fbbfcdec1f1ba5fc2a5be264bba0851ea
Sangey <sangeylama@gmail.com>
2022-05-12 00:57:01 +0200

    Refactored and improved usage stats

--- android/app/src/main/java/dk/mths/jomo/work/BackgroundWorker.kt
+++ android/app/src/main/java/dk/mths/jomo/work/BackgroundWorker.kt
@@ -9,6 +9,8 @@ import androidx.core.app.NotificationManagerCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import dk.mths.jomo.R
+import dk.mths.jomo.utils.StatusNotificationHelper
+import java.io.File
 
 
 class BackgroundWorker(appContext: Context, params: WorkerParameters) :
@@ -25,42 +27,17 @@ class BackgroundWorker(appContext: Context, params: WorkerParameters) :
     val CHANNEL_ID = "VERBOSE_NOTIFICATION"
     val NOTIFICATION_ID = 1
 
+
         override fun doWork(): Result{
             val appContext = applicationContext
+            val notificationHelper = StatusNotificationHelper()
 
-            makeStatusNotification("Background Work", appContext)
+            val testData = inputData.getString("Test")
+
+            notificationHelper.makeStatusNotification(testData.toString(), appContext)
 
             return Result.success()
         }
 
-    fun makeStatusNotification(message: String, context: Context) {
 
-        // Make a channel if necessary
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            // Create the NotificationChannel, but only on API 26+ because
-            // the NotificationChannel class is new and not in the support library
-            val name = VERBOSE_NOTIFICATION_CHANNEL_NAME
-            val description = VERBOSE_NOTIFICATION_CHANNEL_DESCRIPTION
-            val importance = NotificationManager.IMPORTANCE_HIGH
-            val channel = NotificationChannel(CHANNEL_ID, name, importance)
-            channel.description = description
-
-            // Add the channel
-            val notificationManager =
-                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager?
-
-            notificationManager?.createNotificationChannel(channel)
-        }
-
-        // Create the notification
-        val builder = NotificationCompat.Builder(context, CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
-            .setContentTitle(NOTIFICATION_TITLE)
-            .setContentText(message)
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
-            .setVibrate(LongArray(0))
-
-        // Show the notification
-        NotificationManagerCompat.from(context).notify(NOTIFICATION_ID, builder.build())
-    }
 }
