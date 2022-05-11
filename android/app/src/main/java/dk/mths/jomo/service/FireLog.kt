a7d60a84a258a02fec5874a74f7fcd8f15378a6d
Raymond Staniek <rstani20@student.aau.dk>
2022-05-11 19:28:47 +0200

    fixed firelog

--- android/app/src/main/java/dk/mths/jomo/service/FireLog.kt
+++ android/app/src/main/java/dk/mths/jomo/service/FireLog.kt
@@ -1,6 +1,7 @@
 package dk.mths.jomo.service
 
 import android.util.Log
+import com.google.android.gms.tasks.Task
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.installations.FirebaseInstallations
 import com.google.firebase.ktx.Firebase
@@ -28,41 +29,45 @@ open class FireLog {
     }
 
     fun sendLog(tag: String) {
-        injectTrackingData()
-        params[tagKey] = tag
+        injectTrackingData().addOnSuccessListener {
+            params[tagKey] = tag
 
-        Firebase.firestore
-            .collection("logs")
-            .add(params)
-            .addOnSuccessListener { documentReference ->
-                Log.d(firebaseLogTag, "DocumentSnapshot added with ID: ${documentReference.id}")
-            }
-            .addOnFailureListener { e ->
-                Log.wtf(firebaseLogTag, "Error adding document", e)
-            }
+            Firebase.firestore
+                .collection("logs")
+                .add(params)
+                .addOnSuccessListener { documentReference ->
+                    Log.d(firebaseLogTag, "DocumentSnapshot added with ID: ${documentReference.id}")
+                }
+                .addOnFailureListener { e ->
+                    Log.wtf(firebaseLogTag, "Error adding document", e)
+                }
+        }
     }
 
     fun sendError(e: Exception) {
-        injectTrackingData()
-        params[tagKey] = "ERROR"
-        params["error_message"] = e.localizedMessage as Any
-        params["stacktrace"] = e.stackTraceToString()
+        injectTrackingData().addOnSuccessListener {
+            params[tagKey] = "ERROR"
+            params["error_message"] = e.localizedMessage as Any
+            params["stacktrace"] = e.stackTraceToString()
 
-        Firebase.firestore
-            .collection("errors")
-            .add(params)
-            .addOnSuccessListener { documentReference ->
-                Log.d(firebaseLogTag, "DocumentSnapshot added with ID: ${documentReference.id}")
-            }
-            .addOnFailureListener { e ->
-                Log.wtf(firebaseLogTag, "Error adding document", e)
-            }
+            Firebase.firestore
+                .collection("errors")
+                .add(params)
+                .addOnSuccessListener { documentReference ->
+                    Log.d(firebaseLogTag, "DocumentSnapshot added with ID: ${documentReference.id}")
+                }
+                .addOnFailureListener { e ->
+                    Log.wtf(firebaseLogTag, "Error adding document", e)
+                }
+        }
     }
 
-    private fun injectTrackingData(){
-        params[userIdKey] = FirebaseInstallations.getInstance().id.result
-        if(!params.containsKey(timestampKey)){
-            params[timestampKey] = DateTimeFormatter.ISO_INSTANT.format(Instant.now())
+    private fun injectTrackingData(): Task<String>{
+        return FirebaseInstallations.getInstance().id.addOnSuccessListener { firebaseInstanceId ->
+            params[userIdKey] = firebaseInstanceId
+            if(!params.containsKey(timestampKey)){
+                params[timestampKey] = DateTimeFormatter.ISO_INSTANT.format(Instant.now())
+            }
         }
     }
 }
