9226ea1f96ff2f7b1113fc7fb2d7ee889c4b056c
Sangey <sangeylama@gmail.com>
2022-05-12 00:57:01 +0200

    Refactored and improved usage stats

---
+++ android/app/src/main/java/dk/mths/jomo/utils/StatusNotificationHelper.kt
@@ -0,0 +1,51 @@
+package dk.mths.jomo.utils
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import dk.mths.jomo.R
+
+class StatusNotificationHelper {
+
+    @JvmField val VERBOSE_NOTIFICATION_CHANNEL_NAME: CharSequence =
+        "Verbose WorkManager Notifications"
+    val VERBOSE_NOTIFICATION_CHANNEL_DESCRIPTION =
+        "Shows notifications whenever work starts"
+    @JvmField val NOTIFICATION_TITLE: CharSequence = "Test WorkRequest Starting"
+    val CHANNEL_ID = "VERBOSE_NOTIFICATION"
+    val NOTIFICATION_ID = 1
+
+    fun makeStatusNotification(message: String, context: Context) {
+
+        // Make a channel if necessary
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create the NotificationChannel, but only on API 26+ because
+            // the NotificationChannel class is new and not in the support library
+            val name = VERBOSE_NOTIFICATION_CHANNEL_NAME
+            val description = VERBOSE_NOTIFICATION_CHANNEL_DESCRIPTION
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(CHANNEL_ID, name, importance)
+            channel.description = description
+
+            // Add the channel
+            val notificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager?
+
+            notificationManager?.createNotificationChannel(channel)
+        }
+
+        // Create the notification
+        val builder = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(NOTIFICATION_TITLE)
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setVibrate(LongArray(0))
+
+        // Show the notification
+        NotificationManagerCompat.from(context).notify(NOTIFICATION_ID, builder.build())
+    }
+}
