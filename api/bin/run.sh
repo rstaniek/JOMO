edff25d130fccc38c0681cad04369eb5eb3223f0
Raymond Staniek <rstani20@student.aau.dk>
2022-05-14 11:54:09 +0200

    zzz

--- api/bin/run.sh
+++ api/bin/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-gunicorn wsgi:app --bind 0.0.0.0:8808 --log-level=debug --workers=2 --certfile /var/www/jomo-api.pem --keyfile /var/www/jomo-api.key
+gunicorn --certfile /var/www/jomo-api.pem --keyfile /var/www/jomo-api.key --bind 0.0.0.0:8808 --log-level=debug --workers=1 wsgi:app
