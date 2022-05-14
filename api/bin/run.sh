781de9c81b21baeb6c42a014474e30e3689dcbce
Raymond Staniek <rstani20@student.aau.dk>
2022-05-14 11:48:12 +0200

    wrong port

--- api/bin/run.sh
+++ api/bin/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-gunicorn wsgi:app --bind 0.0.0.0:443 --log-level=debug --workers=2 --certfile /var/www/jomo-api.pem --keyfile /var/www/jomo-api.key
+gunicorn wsgi:app --bind 0.0.0.0:8808 --log-level=debug --workers=2 --certfile /var/www/jomo-api.pem --keyfile /var/www/jomo-api.key
